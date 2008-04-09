ccda9ea211054b3548986799e9a4a1b1d3a6e1b4
DanielSchurter <DanielSchurter@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-09 13:38:07 +0000

    update...
    git-svn-id: https://plugins.svn.wordpress.org/dmsguestbook/tags/1.10.0@39420 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ template/form/default.tpl
@@ -0,0 +1,125 @@
+<?php
+/*---------------------------------------------------------------------------
+Be free to change what you want... @ your own risk :-)
+Would you like to use your own template?
+1.) Copy this code in a other file and save it with the .tpl prefix (e.g. myfile.tpl)
+3.) Customize your template and save it
+2.) Select your template on DMSGuestbook admin page for use
+
+Guestbook input form
+
+CSS variables:
+css_form_textfieldspace				= define space between each text fields
+css_form_errormessage				= wrong input text error message
+css_form_successmessage				= success input text message
+css_form_namefield					= name field
+css_form_emailfield					= email field
+css_form_urlfield					= url field
+css_form_messagefield				= message field
+css_form_antispamtext				= antispam text
+css_form_antispamcontent				= antispam image or mathematic figures
+css_form_antispamcontent_position	= antispam image or mathematic figures position
+css_form_antispam_inputfield			= antispam input field
+css_form_submit_position				= submit button position
+Edit these CSS settings on DMSGuestbook admin panel (CSS section)
+
+Function variables:
+$error1							= name error
+$error2							= email error
+$error3							= url error
+$error4							= message error
+$error5							= antispam error
+$success						= success message
+$gbname							= name
+$gbemail						= email
+$gburl							= url
+$gbmsg							= message
+$captcha1						= visual captcha
+$captcha2						= mathematical captcha
+$var_mandatory_char				= mandatory char
+$var_mandatory_email				= display $var_mandatory_char if it set in the admin page
+$var_mandatory_url				= display $var_mandatory_char if it set in the admin page
+Edit these variables on DMSGuestbook admin panel
+
+Language text variables:
+$lang_name						= name title
+$lang_email						= email title
+$lang_message					= message title
+$lang_require					= require title
+$lang_antispam					= antispam description text
+$lang_submit					= submit button title
+Edit these variables on DMSGuestbook admin panel
+---------------------------------------------------------------------------*/
+
+		$var_form1 = "
+				<!-- define the space on top of the form -->
+				<div class='css_form_textfieldspace'></div>
+
+				<!-- error & succes messages -->
+				<div class='css_form_errormessage'>$error1</div>
+				<div class='css_form_errormessage'>$error2</div>
+				<div class='css_form_errormessage'>$error3</div>
+				<div class='css_form_errormessage'>$error4</div>
+				<div class='css_form_errormessage'>$error5</div>
+				<div class='css_form_successmessage'>$success</div>
+				<br />
+				";
+
+
+		$var_form2 = "
+				<!-- name field -->
+				<div class='css_form_textfieldspace'>
+				<input class='css_form_namefield' type='text' name='gbname' value='$gbname' maxlength='50' />&nbsp;$lang_name $var_mandatory_char</div>
+
+				<!-- email field -->
+				<div class='css_form_textfieldspace'>
+				<input class='css_form_emailfield' type='text' name='gbemail' value='$gbemail' />&nbsp;$lang_email $var_mandatory_email</div>
+
+				<!-- url field -->
+				<div class='css_form_textfieldspace'>
+				<input class='css_form_urlfield' type='text' name='gburl' value='$gburl' />&nbsp;$lang_url $var_mandatory_url</div>
+
+				<!-- message field -->
+				<div class='css_form_textfieldspace'>
+				<textarea class='css_form_messagefield' name='gbmsg' rows='0' cols='0'>$gbmsg</textarea>&nbsp;$lang_message $var_mandatory_char</div>
+
+				<!-- require text and the requirement char -->
+				<div style='text-align:left;'>$var_mandatory_char $lang_require</div>
+				<br />
+				";
+
+
+		$var_form3 = "
+				<!-- visual captcha if selected -->
+				<div class='css_form_antispamtext'>$lang_antispam</div>
+				<div class='css_form_antispamcontent_position'>
+				<img class='css_form_antispamcontent' src='$captcha1' alt='captcha' /></div>
+				<div class='css_form_antispamcontent_position'>
+				<input class='css_form_antispam_inputfield' type='text' name='securecode' /></div>
+				";
+
+
+		$var_form4 = "
+				<!-- mathematical chaptcha if selected -->
+				<div class='css_form_antispamtext'>$lang_antispam</div>
+				$captcha2 <input class='css_form_antispam_inputfield' type='text' name='securecode' />
+				";
+
+
+		$var_form5 = "
+				<!-- submit button -->
+				<div class='css_form_submit_position'><input class='css_form_submit' type='submit' value='$lang_submit' /></div>
+				";
+
+
+		$var_form6 = "
+				<!-- define space after the submit button -->
+				<p style='padding:10px 0px 0px 0px;'></p>
+				";
+
+
+		$var_form7 = "
+				<!-- define space between form and navigation -->
+				<div style='padding:30px 0px 0px 0px;'></div>
+				";
+?>
---
+++ widget.php
@@ -0,0 +1,175 @@
+<?php
+/*
+Plugin Name: DMSGuestbook widget
+Plugin URI: http://DanielSchurter.net
+Description: Add a DMSGuestbook widget to your sidebar.
+Author: Daniel M. Schurter
+Version: 1.2
+Author URI: http://DanielSchurter.net
+*/
+
+
+/* initializing */
+function widget_dmsguestbook_init() {
+	if ( !function_exists('register_sidebar_widget') || !function_exists('register_widget_control') )
+		return;
+	function widget_dmsguestbook_control() {
+		$options = $newoptions = get_option('widget_dmsguestbook');
+		if ( !is_array($newoptions) )
+			$newoptions = array(
+				'title' => 'Guestbook',
+				'entries'=> '5',
+				'wordcut'=> '25',
+				'namecss1'=> '<b style="font-weight:bold;">',
+				'namecss2'=> '</b>',
+				'messagecss1'=> '<b style="font-weight:normal;">',
+				'messagecss2'=> '</b>',
+				'frame1'=> '<div style="background-color:#F7CDC1; padding:5px; border:1px dashed #dd8888;">',
+				'frame2'=> '</div>');
+
+			/* check the old HTTP_POST_VARS and new $_POST var */
+			if(!empty($HTTP_POST_VARS)) {
+			$POSTVARIABLE   = $HTTP_POST_VARS;
+			}
+			else {
+		 	     $POSTVARIABLE = $_POST;
+		 	     }
+
+			if ( $POSTVARIABLE['submit'] ) {
+
+				/* prevent XSS */
+				$remove_tags="(\<textarea\>||\<\/textarea\>)";
+
+				$newoptions['title'] = mysql_real_escape_string(preg_replace("/[[:punct:]]/", "", $POSTVARIABLE['DMSGuestbook_title']));
+				$newoptions['entries'] = sprintf("%d", $POSTVARIABLE['DMSGuestbook_entries']);
+				$newoptions['wordcut'] = sprintf("%d", $POSTVARIABLE['DMSGuestbook_wordcut']);
+				$newoptions['namecss1'] = mysql_real_escape_string(preg_replace("/$remove_tags/", "", $POSTVARIABLE['DMSGuestbook_namecss1']));
+				$newoptions['namecss2'] = mysql_real_escape_string(preg_replace("/$remove_tags/", "", $POSTVARIABLE['DMSGuestbook_namecss2']));
+				$newoptions['messagecss1'] = mysql_real_escape_string(preg_replace("/$remove_tags/", "", $POSTVARIABLE['DMSGuestbook_messagecss1']));
+				$newoptions['messagecss2'] = mysql_real_escape_string(preg_replace("/$remove_tags/", "", $POSTVARIABLE['DMSGuestbook_messagecss2']));
+				$newoptions['frame1'] = mysql_real_escape_string(preg_replace("/$remove_tags/", "", $POSTVARIABLE['DMSGuestbook_frame1']));
+				$newoptions['frame2'] = mysql_real_escape_string(preg_replace("/$remove_tags/", "", $POSTVARIABLE['DMSGuestbook_frame2']));
+				}
+
+			if ($options != $newoptions) {
+				$options = $newoptions;
+				update_option('widget_dmsguestbook', $options);
+				}
+
+
+		echo '<table><tr>';
+		echo '<td>Title:</td>
+		<td><input style="width:150px;" id="DMSGuestbook_title" name="DMSGuestbook_title" type="text" value="'.str_replace("\\", "", $options['title']).'" /></td><tr>';
+		echo '<tr><td>Number of post to show:</td>
+		<td><input style="width:30px;" type="text" name="DMSGuestbook_entries" value="'.$options['entries'].'"></td></tr>';
+		echo '<tr><td>Cut message text after </td>
+		<td><input style="width:30px;" type="text" name="DMSGuestbook_wordcut" value="'.$options['wordcut'].'"> characters</td></tr>';
+
+		echo "<tr><td></td><td><br />Use HTML & CSS elements to customize your settings.<br />
+		Don't forget to close all tags!<br />
+		</td></tr>";
+
+		echo '<td>Header frame:</td>
+		<td><textarea style="width:400px;height:50px;background-color:#C4D3FF;" name="DMSGuestbook_frame1" />'.str_replace("\\","",$options['frame1']).'</textarea>
+		</td><tr>';
+
+		echo '<td>Name CSS:</td>
+		<td><textarea style="width:400px;height:80px;background-color:#DCFFC4;" name="DMSGuestbook_namecss1" />'.str_replace("\\","",$options['namecss1']).'</textarea>
+		<textarea style="width:400px;height:20px;background-color:#DCFFC4;" name="DMSGuestbook_namecss2" />'.str_replace("\\","",$options['namecss2']).'</textarea>
+		</td><tr>';
+
+		echo '<td>Message CSS:</td>
+		<td><textarea style="width:400px;height:80px;background-color:#FFF3C4;" name="DMSGuestbook_messagecss1" />'.str_replace("\\","",$options['messagecss1']).'</textarea>
+		<textarea style="width:400px;height:20px;background-color:#FFF3C4;" name="DMSGuestbook_messagecss2" />'.str_replace("\\","",$options['messagecss2']).'</textarea>
+		</td><tr>';
+
+		echo '<td>Footer frame:</td>
+		<td><textarea style="width:400px;height:50px;background-color:#C4D3FF;" name="DMSGuestbook_frame2" />'.str_replace("\\","",$options['frame2']).'</textarea>
+		</td><tr>';
+
+		echo '<input type="hidden" id="submit" name="submit" value="1" />';
+
+		/* describe options */
+		echo "<tr><td></td><td><br /><b>Options</b><br />
+		<b style='font-weight:normal;font-size:11px;'>Use this tags in name and message field<br /><br />
+		Direct link to post: SHOW_POST<br />
+		Example: &lt;a href=\"http://my-guestbook-link/?SHOW_POST\"&gt;<br />
+		Example: &lt;a href=\"http://my-guestbook-link/?page_id=123&SHOW_POST\"&gt;<br />
+		<br />
+		Show number: SHOW_NR<br />
+		Example: &lt;b&gt;(SHOW_POST)&lt;/b&gt;<br />
+		<br />
+		Show databse id: SHOW_ID<br />
+		Example: &lt;b&gt;(SHOW_ID)&lt;/b&gt;</b><br />
+		<br />
+		</td></tr>";
+
+		echo '</table>';
+	}
+
+/* what you see in the side of your webpage */
+function widget_dmsguestbook($args) {
+	extract($args);
+	$options = get_option('widget_dmsguestbook');
+	$title = str_replace("\\", "", $options['title']);
+	$entries = $options['entries'];
+	$wordcut = $options['wordcut'];
+	$namecss1 = str_replace("\\", "", $options['namecss1']);
+	$namecss2 = str_replace("\\", "", $options['namecss2']);
+	$messagecss1 = str_replace("\\", "", $options['messagecss1']);
+	$messagecss2 = str_replace("\\", "", $options['messagecss2']);
+	$frame1 = str_replace("\\", "", $options['frame1']);
+	$frame2 = str_replace("\\", "", $options['frame2']);
+
+	echo "<!-- Start DMSGuestbook widget -->\n";
+	echo $before_widget . $before_title . $title . $after_title . "<br />";
+
+		global $wpdb;
+		$table_name = $wpdb->prefix . "dmsguestbook";
+			/* read options, use ASC or DESC */
+			$options = get_option('DMSGuestbook_options');
+			$part1 = explode("<sortitem>", $options);
+			$part2 = explode("</sortitem>", $part1[1]);
+
+		$query = $wpdb->get_results("SELECT id, name, message FROM $table_name WHERE flag != '1' ORDER BY id
+		" . sprintf("%s", $part2[0]) . " LIMIT " . sprintf("%d", $entries) . "");
+
+		echo $frame1;
+		$itemnr=0;
+		foreach ($query as $result) {
+
+			/* rewrite tags */
+			$namecss11		=str_replace("SHOW_POST", "from=$itemnr&amp;widget_gb_step=1&amp;select=1", $namecss1);
+			$messagecss11 	=str_replace("SHOW_POST", "from=$itemnr&amp;widget_gb_step=1&amp;select=1", $messagecss1);
+
+			$namecss12 		=str_replace("SHOW_ID", "$result->id", $namecss11);
+			$messagecss12 	=str_replace("SHOW_ID", "$result->id", $messagecss11);
+
+			$namecss13		=str_replace("SHOW_NR", $itemnr+1, $namecss12);
+			$messagecss13	=str_replace("SHOW_NR", $itemnr+1, $messagecss12);
+
+			$itemnr++;
+			/* end rewrite tags */
+
+			echo $namecss13 . stripslashes($result->name) . $namecss2 . "<br />";
+
+			$message = str_replace("[html]", "", $result->message);
+			$message = str_replace("[/html]", "", $message);
+
+			if($wordcut!=0) {
+			echo $messagecss13 . substr(str_replace("\\","",stripslashes(strip_tags($message))), 0, $wordcut) . $messagecss2 . "..." ."<br /><br />";
+			} else {echo $messagecss13 . strip_tags(str_replace("\\","",$message)) . $messagecss2 . "<br /><br />";}
+		}
+		echo $frame2;
+
+
+	echo $after_widget;
+	echo "\t<!-- Stop DMSGuestbook widget -->\n";
+	}
+
+
+register_sidebar_widget('DMSGuestbook', 'widget_dmsguestbook');
+register_widget_control('DMSGuestbook', 'widget_dmsguestbook_control', 600, 800);
+}
+add_action('plugins_loaded', 'widget_dmsguestbook_init');
+?>
